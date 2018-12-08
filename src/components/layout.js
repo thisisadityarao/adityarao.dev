5b3033906d6b48a6df6893335ce39eaa5e76964d
Aditya Rao <dev.adityarao@gmail.com>
2018-12-08 22:23:31 +0530

    Add overflow-y property to body

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,6 +30,7 @@ const GlobalStyle = createGlobalStyle`
   font-family: "Nunito Sans", sans-serif;
   font-size: ${modularScale(0, '1em', 'majorThird')};
   line-height: 1.4;
+  overflow-y: hidden;
   }
 
   p {
