37addcb2469672aec9d50b4d7ebeeb921ad5e0f7
Aditya Rao <dev.adityarao@gmail.com>
2018-12-08 22:15:34 +0530

    Remove overflow property from body

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,7 +30,6 @@ const GlobalStyle = createGlobalStyle`
   font-family: "Nunito Sans", sans-serif;
   font-size: ${modularScale(0, '1em', 'majorThird')};
   line-height: 1.4;
-  overflow: hidden;
   }
 
   p {
