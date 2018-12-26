c092ac8a0135eb8b6de2212af37ea61b74770818
Aditya Rao <dev.adityarao@gmail.com>
2018-12-26 20:52:17 +0530

    Change display property on Footer to position

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Footer = styled.footer`
   height: 100px;
   width: 100%;
-  display: fixed;
+  position: fixed;
   bottom: 0;
   left: 0;
   background-color: #fafafa;
