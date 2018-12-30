a8f1713839ce1c706f3110072c44905c7e0a5404
Aditya Rao <dev.adityarao@gmail.com>
2018-12-30 12:26:53 +0530

    Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,48 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
-        icons: 'src/images/icon.png', // This path is relative to the root of the site.
+        icons: [
+          {
+            src: 'static/icons/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: 'static/icons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: 'static/icons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: 'static/icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: 'static/icons/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+          },
+          {
+            src: 'static/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'static/icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: 'static/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ], // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
