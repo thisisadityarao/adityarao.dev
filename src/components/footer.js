f9c536de71b8cc01405efcda0109c25499beacb7
Aditya Rao <dev.adityarao@gmail.com>
2019-01-02 21:24:53 +0530

    Change footer text.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,25 +2,45 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Footer = styled.footer`
-  height: 100px;
+  height: 120px;
   width: 100%;
   border: 1px solid #eeeeee;
 
-  & small {
+  & p {
     font-size: 80%;
-    display: block;
     margin: auto;
-    margin-top: 40px;
+    margin-top: 50px;
     text-align: center;
-    color: #000;
+    line-height: 1.5;
+  }
+
+  & a:link {
+      text-decoration: underline;
+      padding-bottom: 1px;
+      color: #f43059;
+    }
+
+  & a:focus,
+  & a:hover {
+    color: #ff4b6e;
+  }
+
+  & a:active {
+    color: #f43059;
+  }
+
+  & a:visited {
+    color: #f43059;
   }
 `;
 
 export default () => (
   <Footer>
-    <small>
-      <span style={{ color: '#f43059' }}>&copy;</span>2018 All rights reserved - Designed and coded
-      by Aditya 👨🏽‍💻
-    </small>
+    <p>
+      Build with <a href="https://www.gatsbyjs.org/">Gatsby</a> and{' '}
+      <a href="https://reactjs.org/">React</a>. Hosted on{' '}
+      <a href="https://www.netlify.com/">Netlify</a>. The code is open source and available at{' '}
+      <a href="https://github.com/ad1tyara0/raoaditya.com">Github</a>.
+    </p>
   </Footer>
 );
