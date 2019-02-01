69ad32c931468f98757ca0048592ee5cbccc8dda
Aditya Rao <dev.adityarao@gmail.com>
2019-02-01 19:58:54 +0530

    Add smooth animation to scroll-to-top component

--- src/components/scroll-to-top.js
+++ src/components/scroll-to-top.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import detectPassiveEvents from 'detect-passive-events';
+import TweenFunctions from 'tween-functions';
 import { ChevronCircleUp } from 'styled-icons/fa-solid/ChevronCircleUp';
 
 const ScrollButton = styled(ChevronCircleUp)`
@@ -13,24 +14,22 @@ const ScrollButton = styled(ChevronCircleUp)`
   z-index: 99; /* Make sure it does not overlap */
   border: none; /* Remove borders */
   outline: none; /* Remove outline */
-  background-color: #555; /* Set a background color */
-  background: #00bcd4;
-  color: white; /* Text color */
+  background-color: #fff; /* Set a background color */
+  color: #555; /* Text color */
   cursor: pointer; /* Add a mouse pointer on hover */
   width: 35px; /* Some width */
   font-size: 18px; /* Increase font size */
   border-radius: 20px;
-  border: 2px solid #00bcd4;
 
   &:hover {
-    background: #f43059;
-    border-color: #f43059;
+    color: #f43059;
   }
 
   @media (min-width: 786px) {
     bottom: 30px;
     right: 30px;
     width: 40px;
+    font-size: 16px;
   }
 `;
 
@@ -38,25 +37,20 @@ export default class ScrollToTop extends Component {
   constructor(props) {
     super(props);
     this.state = { isScrolling: '' };
+    this.Animation = {
+      StartPosition: 0, // Passed as an argument to easing function to calculate next position
+      CurrentAnimationTime: 0, // Passed as an argument to easing function to calculate next position
+      StartTime: null, // Needed to calculate currentAnimationTime
+      AnimationFrame: null, // RefID that requestAnimationFrame returns
+    };
     this.ButtonToggle = this.ButtonToggle.bind(this);
-    this.Scrolling = this.Scrolling.bind(this);
-  }
-
-  ButtonToggle() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-      this.setState({ isScrolling: true });
-    } else {
-      this.setState({ isScrolling: '' });
-    }
-  }
-
-  Scrolling() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    this.HandleScrolling = this.HandleScrolling.bind(this);
+    this.StopScrollingFrame = this.StopScrollingFrame.bind(this);
+    this.ScrollingFrame = this.ScrollingFrame.bind(this);
   }
 
   componentDidMount() {
-    this.Scrolling(); // Scroll to top in case we land in the middle of the page
+    this.HandleScrolling(); // Scroll to top at mount incase we are already scrolled down
     if (typeof window !== 'undefined') {
       window.addEventListener('scroll', this.ButtonToggle);
     }
@@ -76,12 +70,8 @@ export default class ScrollToTop extends Component {
     }
   }
 
-  render() {
-    const { isScrolling } = this.state;
-    return <ScrollButton onClick={this.Scrolling} customProp={isScrolling} />;
-  }
-
   componentWillUnmount() {
+    // Remove all events, since component is no longer mounted.
     if (typeof window !== 'undefined') {
       window.removeEventListener('scroll', this.ButtonToggle);
     }
@@ -91,5 +81,74 @@ export default class ScrollToTop extends Component {
     if (typeof window !== 'undefined') {
       window.removeEventListener('touchstart', this.StopScrollingFrame, false);
     }
+  }
+
+  ButtonToggle() {
+    // Position at which to show scroll-to-top button(unit: px)
+    // Can convert it into a prop??
+    const ShowAtPosition = 900;
+    // window.pageYOffset = current scroll position
+    // ShowAtPosition = position at which we want the button to show.
+    if (window.pageYOffset > ShowAtPosition) {
+      this.setState({ isScrolling: true });
+    } else {
+      this.setState({ isScrolling: '' });
+    }
+  }
+
+  HandleScrolling() {
+    // Scroll to StopPosition
+    this.StopScrollingFrame(); // Stoping all AnimationFrames
+    this.Animation.StartPosition = window.pageYOffset; // current scroll position
+    this.Animation.CurrentAnimationTime = 0;
+    this.Animation.StartTime = null;
+    // Start the scrolling animation.
+    this.Animation.AnimationFrame = window.requestAnimationFrame(this.ScrollingFrame);
+  }
+
+  ScrollingFrame() {
+    // Where to stop the scroll animation(end position: pageYOffset = 0)
+    // Can convert it into a prop??
+    const StopPosition = 0;
+    // Total duration for animation
+    // Can convert it into a prop??
+    const AnimationDuration = 500;
+    const timestamp = Math.floor(Date.now());
+    // If StartTime has not been assigned a value, assign it the start timestamp.
+    if (!this.Animation.StartTime) {
+      this.Animation.StartTime = timestamp;
+    }
+
+    // set CurrentAnimationTime every iteration of ScrollingFrame()
+    this.Animation.CurrentAnimationTime = timestamp - this.Animation.StartTime;
+    // if we hit the StopPosition, StopScrollingFrame()
+    if (window.pageYOffset <= StopPosition) {
+      this.StopScrollingFrame();
+    } else {
+      // Otherwise continue ScrollingFrame to the StopPosition.
+      // Let TweenFunctions handle the math to give us a new position based on AnimationDuration and EasingType type
+      let YPos = TweenFunctions.easeOutCubic(
+        this.Animation.CurrentAnimationTime,
+        this.Animation.StartPosition,
+        StopPosition,
+        AnimationDuration,
+      );
+      if (YPos <= StopPosition) {
+        YPos = StopPosition;
+      }
+      window.scrollTo(0, YPos);
+      // Request another frame to be painted
+      this.Animation.AnimationFrame = window.requestAnimationFrame(this.ScrollingFrame);
+    }
+  }
+
+  StopScrollingFrame() {
+    // Stop the Animation Frames.
+    window.cancelAnimationFrame(this.Animation.AnimationFrame);
+  }
+
+  render() {
+    const { isScrolling } = this.state;
+    return <ScrollButton onClick={this.HandleScrolling} customProp={isScrolling} />;
   }
 }
