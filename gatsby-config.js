38f8f94136396b43d35a84a2fcd5d53bec63285f
Aditya Rao <dev.adityarao@gmail.com>
2019-03-03 12:20:30 +0530

    Change background color and theme color for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@ module.exports = {
         name: 'Aditya Rao',
         short_name: 'Aditya Rao',
         start_url: '/',
-        background_color: '#FFB302',
-        theme_color: '#FFB302',
+        background_color: '#FA8E86',
+        theme_color: '#FA8E86',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
