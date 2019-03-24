54e6f45353d6cfc132ef96bf206cbdf04ad6d6c0
Aditya Rao <dev.adityarao@gmail.com>
2019-03-24 16:40:25 +0530

    Change theme color in gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@ module.exports = {
         name: 'Aditya Rao',
         short_name: 'Aditya Rao',
         start_url: '/',
-        background_color: '#E76F03',
-        theme_color: '#E76F03',
+        background_color: '#5e80de',
+        theme_color: '#5e80de',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
