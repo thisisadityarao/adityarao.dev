e6d902ef33a30be4b5c00b1d27882c88c5192988
Aditya Rao <dev.adityarao@gmail.com>
2019-12-18 05:49:11 +0530

    Update dependencies

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,3 +11,17 @@ require('typeface-bree-serif');
 require('prism-themes/themes/prism-dracula.css');
 require('prismjs/plugins/line-numbers/prism-line-numbers.css');
 require('./static/styles/override-prism.css');
+
+/* global window, document */
+const scrollTo = id => () => {
+  const el = document.querySelector(id);
+  if (el) return window.scrollTo(0, el.offsetTop - 20);
+  return false;
+};
+
+export const onRouteUpdate = ({ location: { hash } }) => {
+  console.log(location);
+  if (hash) {
+    window.setTimeout(scrollTo(hash), 10);
+  }
+};
